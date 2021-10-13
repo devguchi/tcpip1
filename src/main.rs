e46ff50c95b31238e63d18683cabd05452b40d5d
devguchi <devguchitaro@gmail.com>
2021-10-13 23:15:02 +0900

    added args (protocol, role, address)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::env;
+use log::{error};
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+        error!("please input 3 args.");
+        std::process::exit(1);
+    }
+    let protocol:&str = &args[1];
+    let role:&str = &args[2];
+    let address:&str = &args[3];
+    println!("[protocol] {} [role] {} [address] {}", protocol, role, address);
+    match protocol {
+        "tcp" => match role {
+            "server" => {},
+            "client" => {},
+            _ => {missing_role();},
+        },
+        "udp" => match role {
+            "server" => {},
+            "client" => {},
+            _ => {missing_role();},
+        },
+        _ => {
+            error!("args error.");
+            std::process::exit(1);
+        },
+    }
+}
+
+fn missing_role() {
+    error!("missing role.");
+    std::process::exit(1);
 }
